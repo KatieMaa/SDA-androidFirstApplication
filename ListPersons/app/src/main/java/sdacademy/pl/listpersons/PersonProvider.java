f85e16efd5959a75f3c49755de34dc25b76843c1
Kasia <k.maczkowska@gmail.com>
2017-04-01 13:57:25 +0200

    Added Persons List Project

---
+++ ListPersons/app/src/main/java/sdacademy/pl/listpersons/PersonProvider.java
@@ -0,0 +1,23 @@
+package sdacademy.pl.listpersons;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by RENT on 2017-04-01.
+ */
+
+public class PersonProvider {
+
+    public List<Person> provide() {
+
+        List<Person> result = new ArrayList<>();
+        result.add(new Person("Jan", 22));
+        result.add(new Person("Adam", 12));
+        result.add(new Person("Jakub", 29));
+        result.add(new Person("Grażyna", 21));
+        return result;
+    }
+}
+
+
